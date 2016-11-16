a154d216907b211a742d34cc01eaae0315330b2e
Harold Putman <harold.putman@lexmark.com>
2016-11-16 15:20:47 -0500

    Test multiple good files.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 
 const assert = require('assert');
 const eslint = require('eslint');
+const fs = require('fs');
 
 // This depends on presence of .eslintrc.json point to this repo
 
@@ -15,16 +16,26 @@ function assertReportContains(report, msg) {
 
 describe('Good Lint', () => {
   it('should find nothing wrong with good files.', () => {
-    const report = new eslint.CLIEngine().executeOnFiles(['./test/good/sample.js']);
-    assert.equal(report.errorCount, 0);
-    assert.equal(report.warningCount, 0);
-    assert(report.results[0].filePath.endsWith('good/sample.js'));
+    fs.realpath('./test/good', (err, path) => {
+      var files = fs.readdirSync(path).map((name) => { return `${path}/${name}`; });
+      const report = new eslint.CLIEngine().executeOnFiles(files);
+      assert.equal(report.errorCount, 0);
+      assert.equal(report.warningCount, 0);
+      files.forEach((file, index) => {
+        assert(report.results[index].filePath.endsWith(file));
+      });
+    });
   });
 });
 
 describe('Bad Lint', () => {
   it('should find camelcase errors', () => {
-    const report = new eslint.CLIEngine().executeOnText("var my_favorite_color = '#112C85';");
+    const report = new eslint.CLIEngine().executeOnText('var my_favorite_color = \'#112C85\';');
     assertReportContains(report, 'camelcase');
+  });
+
+  it('should complain about tab characters', () => {
+    const report = new eslint.CLIEngine().executeOnText('if (this) {\n\tthat();\n};');
+    assertReportContains(report, 'no-tabs');
   });
 });
