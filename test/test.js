da74b8cbf849653c1b334f70f499ca821d2b0427
Harold Putman <harold.putman@lexmark.com>
2016-11-16 15:35:47 -0500

    Write Fluent interface to assertion.

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 
 // This depends on presence of .eslintrc.json point to this repo
 
+// Helper function to examine report output
 function assertReportContains(report, msg) {
   let message = report.results[0].messages.reduce((a, m) => {
     return (m.ruleId === msg) ? m : a;
@@ -14,6 +15,18 @@ function assertReportContains(report, msg) {
   assert.notEqual(null, message, 'Report did not contain ' + msg);
 }
 
+// Fluent interface
+function expectReport(report) {
+  return {
+    contains: (msg) => {
+      assertReportContains(report, msg);
+    }
+  };
+}
+
+/**
+ * Put files in the good folder that should pass with no errors
+ */
 describe('Good Lint', () => {
   it('should find nothing wrong with good files.', () => {
     fs.realpath('./test/good', (err, path) => {
@@ -28,14 +41,19 @@ describe('Good Lint', () => {
   });
 });
 
+/**
+ * Rather than test all the rules (which is only testing ESLint), put code
+ * snippets here that we want to flag with some rule and make sure the rule
+ * fires.
+ */
 describe('Bad Lint', () => {
   it('should find camelcase errors', () => {
     const report = new eslint.CLIEngine().executeOnText('var my_favorite_color = \'#112C85\';');
-    assertReportContains(report, 'camelcase');
+    expectReport(report).contains('camelcase');
   });
 
   it('should complain about tab characters', () => {
     const report = new eslint.CLIEngine().executeOnText('if (this) {\n\tthat();\n};');
-    assertReportContains(report, 'no-tabs');
+    expectReport(report).contains('no-tabs');
   });
 });
